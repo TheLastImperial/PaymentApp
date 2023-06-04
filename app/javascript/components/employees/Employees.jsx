a8c8f15eeed7e3b9ed8dcac298da3019d0548e4b
thelastimperial <thelastimperial@thelastimperial.com>
2023-06-04 15:05:53 -0700

    Added search to Employee view.

--- app/javascript/components/employees/Employees.jsx
+++ app/javascript/components/employees/Employees.jsx
@@ -1,41 +1,54 @@
-import React, { useEffect } from 'react'
-import { EmployeeItem } from './EmployeeItem'
+import React, { useEffect, useReducer, useRef, useState } from 'react'
 import { useAsync } from 'react-async-hook'
 import { useNavigate } from 'react-router-dom'
+import { EmployeeList } from './EmployeeList'
 
 const fetchEmployees = async( ) => {
   return (await fetch(`/api/v1/employees.json`)).json();
 }
 
 export const Employees = () => {
-  const { loading, result, execute } = useAsync(fetchEmployees, []);
+  const { loading, result=[], execute } = useAsync(fetchEmployees, []);
+  const [employees, setEmployees] = useState(result);
   const navigate = useNavigate();
+  const searchInput = useRef();
 
+  const onSearch = (value) =>{
+    if(!value || value.lenght  === 0){
+      setEmployees(result)
+    }else{
+      setEmployees(employees.filter(({ name }) => name.toLowerCase().includes(value.toLowerCase())))
+    }
+  }
+
+  useEffect(() => {
+    onSearch(searchInput.value)
+  }, [loading])
+  
   return (
     <>
-      <div className="mb-2 d-flex justify-content-end">
-        <button type="button"
-          onClick={ () => navigate("/employees/new", {state: { refetch: execute}}) }
-          className="btn btn-primary">
-          Nuevo empleado
-        </button>
+      <div className="row mb-4">
+        <div className="col-6">
+          <input type="text"
+            className="form-control"
+            placeholder="Buscar"
+            ref={ searchInput }
+            onChange={ (e)=> { onSearch(e.target.value) } }/>
+        </div>
+        <div className="col-6 d-flex justify-content-end">
+          <button type="button"
+            onClick={ () => navigate("/employees/new", {state: { refetch: execute}}) }
+            className="btn btn-primary">
+            Nuevo empleado
+          </button>
+        </div>
       </div>
       {
         !loading &&
         (
-          <div className="pt-6">
-            <ul className="list-group">
-              {
-                result.map(employee => (
-                  <EmployeeItem key={ employee.id }
-                    id={ employee.id }
-                    name={employee.name}
-                    refetch={ execute }
-                  />
-                ))
-              }
-            </ul>
-          </div>
+          <EmployeeList employees={ employees }
+            refetch = { execute }
+          />
         )
       }
     </>
