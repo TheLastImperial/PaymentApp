0717154ffbb7e4852ab6877682a34515703b4a9e
thelastimperial <thelastimperial@thelastimperial.com>
2023-06-04 15:09:59 -0700

    Delete payslips and payslips details.

--- app/models/payslip.rb
+++ app/models/payslip.rb
@@ -1,5 +1,5 @@
 class Payslip < ApplicationRecord
   belongs_to :employee
-  has_many :payslip_details
+  has_many :payslip_details, dependent: :delete_all
   accepts_nested_attributes_for :payslip_details
 end
