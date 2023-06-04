3a385f599cbe11b3c4ad56b5ecda312b4953e83a
thelastimperial <thelastimperial@thelastimperial.com>
2023-06-04 12:40:11 -0700

    Routes and seed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :payslips_details
+      resources :payslips
+      resources :salary_details do
+        collection do
+          get "generic", action: "generic" 
+        end
+      end
+      resources :roles
+      resources :employees      
+    end
+  end
+
   root 'main_page#index'
+  get '/*path' => 'main_page#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,75 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Role.create!([
+    { name: "Chofer" },
+    { name: "Cargador" },
+    { name: "Auxiliar" }
+])
+
+Employee.create!([
+    { name: "Juan", role_id: 1},
+    { name: "Pedro", role_id: 2},
+    { name: "Flor", role_id: 3}
+])
+
+SalaryDetail.create!([
+    {
+        name: "Pago por hora",
+        value: 30,
+        conditional_value: 0,
+        is_quantity: true,
+        is_discount: false,
+        is_main: true,
+    },
+    {
+        name: "Bono por entrega",
+        value: 5,
+        conditional_value: 0,
+        is_quantity: true,
+        is_discount: false,
+        is_main: false,
+    },
+    {
+        name: "Hora extra chofer",
+        value: 10,
+        conditional_value: 192,
+        is_quantity: true,
+        is_discount: false,
+        is_main: false,
+        role_id: 1
+    },
+    {
+        name: "Hora extra cargador",
+        value: 5,
+        conditional_value: 192,
+        is_quantity: true,
+        is_discount: false,
+        is_main: false,
+        role_id: 2
+    },
+    {
+        name: "Vale de despensa",
+        value: 4,
+        conditional_value: 0,
+        is_quantity: false,
+        is_discount: false,
+        is_main: false,
+    },
+    {
+        name: "ISR",
+        value: 9,
+        conditional_value: 0,
+        is_quantity: false,
+        is_discount: true,
+        is_main: false,
+    },
+    {
+        name: "ISR Extra",
+        value: 3,
+        conditional_value: 10000,
+        is_quantity: false,
+        is_discount: true,
+        is_main: false,
+    },
+])
