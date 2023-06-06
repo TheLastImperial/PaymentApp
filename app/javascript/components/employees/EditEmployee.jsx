47f55bebbfcb577d379c6e119900aa7f4ca6211c
thelastimperial <thelastimperial@thelastimperial.com>
2023-06-06 11:04:56 -0700

    Added roles in edit employee.

--- app/javascript/components/employees/EditEmployee.jsx
+++ app/javascript/components/employees/EditEmployee.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useAsync } from 'react-async-hook';
 import { useForm } from 'react-hook-form'
 import { useNavigate, useParams } from 'react-router-dom';
@@ -8,14 +8,19 @@ const fetchEmployee = async( id ) => {
     return (await fetch(`/api/v1/employees/${id}.json`)).json();
 }
 
-export const EditEmployee = () => {
-    const { register, handleSubmit, getValues, formState: { errors } } = useForm();
-    const { id } = useParams()
-    const { loading, result=[] } = useAsync(fetchEmployee, [ id ])
+const fetchRoles = async() => {
+    return (await fetch("/api/v1/roles.json")).json()
+}
 
+export const EditEmployee = () => {
+    const { loading:loadingRoles, result:roles=[] } = useAsync(fetchRoles, [])
+    const { register, handleSubmit, getValues, formState: { errors }, setValue } = useForm();
+    const { id } = useParams()
+    const { loading, result:employee } = useAsync(fetchEmployee, [ id ])
+    const [rolesList, setRolesList] = useState([])
     const navigate = useNavigate()
 
-    const onSubmit = ()=>{
+    const onSubmit = useCallback(()=>{
         const emp = {
             "employee":{
                 name: getValues("name")
@@ -34,15 +39,24 @@ export const EditEmployee = () => {
                 navigate("/employees")
             }
         })
-    }
+    }, [ id ], )
+
+    useEffect(() => {
+        if(!loadingRoles)
+            setRolesList(roles)
+
+        if( loading || loadingRoles )
+            return;
+        setValue("role_id", employee.role.id)
+        setValue("name", employee.name)
+    }, [loading, loadingRoles, employee])
 
     return (
         <>
             {
-                !loading &&
                 (
                     <form onSubmit={ handleSubmit( onSubmit ) }>
-                        <input defaultValue={ result.name }
+                        <input 
                             autoComplete="off"
                             placeholder="Nombre"
                             className="form-control"
@@ -52,6 +66,17 @@ export const EditEmployee = () => {
                             <InputInvalidMessage
                             msg="Ingrese el nuevo nombre." />
                         }
+
+                        <select className="form-select mt-2"
+                            { ...register("role_id")}>
+                            {
+                                rolesList.map(({ id, name}) => (
+                                    <option key={ id } value={ id }>
+                                    { name }
+                                    </option>
+                                ))
+                            }
+                        </select>
 
                         <div className="d-flex justify-content-end">
                             <input type="submit"
